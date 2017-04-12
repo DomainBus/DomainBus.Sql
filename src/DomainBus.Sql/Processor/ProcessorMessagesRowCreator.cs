8b3b2f39fa382d24c758ab0f95ebbb61f3824261
Mihai Mogosanu <mike@sapiensworks.com>
2017-04-12 15:20:22 -0400

    fixed sqlite problem

--- src/DomainBus.Sql/Processor/ProcessorMessagesRowCreator.cs
+++ src/DomainBus.Sql/Processor/ProcessorMessagesRowCreator.cs
@@ -14,16 +14,23 @@ namespace DomainBus.Sql.Processor
         protected override void Configure(IConfigureTable<ProcessorMessagesRow> cfg)
         {
             cfg.ColumnSize(d => d.Processor, 75)
-                .PrimaryKey(pk=>pk.OnColumns(d=>d.ArrivalId))
-                .Column(d=>d.ArrivalId,c=>c.AutoIncrement())
+                .Column(d => d.ArrivalId, c => c.AutoIncrement())
+
                 .Index(pk=>pk.OnColumns(d=>d.MessageId,d=>d.Processor).Unique());
             if (this._db.Provider.IsSqlite())
             {
-                cfg .ColumnDbType(d => d.MessageId, SqliteType.Text);
+                cfg.ColumnDbType(d => d.MessageId, SqliteType.Text)
+
+                    //.PrimaryKey(pk => pk.OnColumns(d => d.ArrivalId))
+                    ;
+
             }
             if (this._db.Provider.IsSqlserver())
             {
-                cfg.ColumnSize(d => d.Data, "max");
+                cfg.ColumnSize(d => d.Data, "max")
+                    .PrimaryKey(pk => pk.OnColumns(d => d.ArrivalId))
+                    //.Column(d => d.ArrivalId, c => c.AutoIncrement())
+                ;
             }                                
         }
     }
